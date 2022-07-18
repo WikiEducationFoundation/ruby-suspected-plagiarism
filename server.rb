46a254b4acff52aa255699ac0d3ed14467d2f40e
Sage Ross <ragesoss@gmail.com>
2022-07-18 13:51:52 -0700

    Simple suspected_diffs implementation

--- server.rb
+++ server.rb
@@ -4,17 +4,21 @@ require_relative './database.rb'
 # PORT 8000 for Kubernetes backend on Toolforge
 set :port, 8000
 
-get '/' do
-  'Ohai'
-end
-
 before do
   content_type 'application/json', 'charset' => 'utf-8'
   Database.connect
 end
 
-
-
 after do
   Database.close_connection
 end
+
+get '/' do
+  'Ohai'
+end
+
+get '/suspected_diffs' do
+  CopyrightDiffs.last(100).to_json
+end
+
+
