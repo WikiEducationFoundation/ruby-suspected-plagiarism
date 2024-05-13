dae360a3929f9c28ecd1a195901fb58b3e836423
Sage Ross <ragesoss@gmail.com>
2024-05-13 13:23:24 -0700

    Update to use new Trove database
    Addresses #1

--- database.rb
+++ database.rb
@@ -2,13 +2,14 @@ require 'active_record'
 require 'mysql2'
 require 'yaml'
 
-# This database is owned and populated by the `eranbot` tool.
+# This database is owned and populated as part of the `copypatrol` project.
 # We just need to read from it.
-COPYRIGHT_DATABASE = 's51306__copyright_p'
+DATABASE_HOST = 'qbyvyo2fbjk.svc.trove.eqiad1.wikimedia.cloud'
+COPYRIGHT_DATABASE = 'copypatrol'
 # turn replica.my.cnf into a yml file with `user` and `password` keys
 credentials = YAML.load(File.open('cnf.yml').read)
-USERNAME = credentials['user']
-PASSWORD = credentials['password']
+USERNAME = credentials['trove_user']
+PASSWORD = credentials['trove_password']
 ITHENTICATE_USER = credentials['ithenticate_user']
 ITHENTICATE_PASSWORD = credentials['ithenticate_password']
 
@@ -18,7 +19,7 @@ class Database
       adapter: 'mysql2',
       database: COPYRIGHT_DATABASE,
       encoding: 'utf8',
-      host: "tools.labsdb",
+      host: DATABASE_HOST,
       username: USERNAME,
       password: PASSWORD
     )
@@ -29,9 +30,13 @@ class Database
   end
 end
 
+# Database schema is documented at https://phabricator.wikimedia.org/T293688
+# WikiEducationDashboard only uses these columns: lang, project, diff, ithenticate_id
+
 class CopyrightDiffs < ActiveRecord::Base
-  self.table_name = 'copyright_diffs'
-  self.ignored_columns = ['report']
+  self.table_name = 'diffs'
+  self.primary_key = 'diff_id'
+  self.ignored_columns = []
 
   def page_title
     # This ensures the page title will be able to convert cleanly to JSON.
